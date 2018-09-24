03321b2d0fbec360f0e29b3362cdcf0df95d7ba1
Will Munn <will.munn@focusrite.com>
2018-09-24 13:13:03 +0100

    Standardise indentation

--- lib/drum_rack.rb
+++ lib/drum_rack.rb
@@ -1,22 +1,23 @@
 require 'note_helpers'
 
-class DrumRack
-	def initialize(rack_xml)
-		@rack_xml = rack_xml
-	end
+module ALS
+  class DrumRack
+    def initialize(rack_xml)
+      @rack_xml = rack_xml
+    end
 
-	def rack_xml
-		@rack_xml
-	end
+    def rack_xml
+      @rack_xml
+    end
 
-	def sample_mapping
-		branches = {}
-		@rack_xml.css('DrumBranch').each do |xml|
-			sample_file = xml.css('MultiSamplePart SampleRef FileRef Name').first['Value']
-			note = xml.css('BranchInfo ReceivingNote').first['Value']
-			branches[NoteHelpers::drum_track_note_to_note_name(note)] = sample_file
-		end
-		return branches
-	end
-
+    def sample_mapping
+      branches = {}
+      @rack_xml.css('DrumBranch').each do |xml|
+        sample_file = xml.css('MultiSamplePart SampleRef FileRef Name').first['Value']
+        note = xml.css('BranchInfo ReceivingNote').first['Value']
+        branches[NoteHelpers::drum_track_note_to_note_name(note)] = sample_file
+      end
+      return branches
+    end
+  end
 end
--- lib/note_helpers.rb
+++ lib/note_helpers.rb
@@ -1,16 +1,16 @@
 module NoteHelpers
-	def NoteHelpers.drum_track_note_to_note_name(note)
-		NoteHelpers.midi_converter_array.reverse[note.to_i - 9]
-	end
+  def NoteHelpers.drum_track_note_to_note_name(note)
+    NoteHelpers.midi_converter_array.reverse[note.to_i - 9]
+  end
 
-	def NoteHelpers.midi_converter_array
-		array = []
-		note_names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-		10.times do |i|
-			note_names.each do |n|
-				array.push( n + (i-2).to_s)
-			end
-		end
-		return array
-	end
+  def NoteHelpers.midi_converter_array
+    array = []
+    note_names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+    10.times do |i|
+      note_names.each do |n|
+        array.push( n + (i-2).to_s)
+      end
+    end
+    return array
+  end
 end
